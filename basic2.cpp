e58b67946f05cd4ecb7c24bb97460d20c02dedd8
tienvuiet <tienxinhzai241@GMAIL.COM>
2024-11-19 13:38:29 +0700

    basic2

---
+++ basic2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a, b;	
+    printf("nhap vap gia tri cua a: ");
+	scanf("%d", &a);
+	printf("nhap vap gia tri cua b: ");
+	scanf("%d", &b);
+	int c=a+b;
+	printf("tong hai so la %d\n ",c);    //d?khfajfdkjsac 
+	return 0; 
+} 
