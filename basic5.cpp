5170be029820b771cd715939f1859d27b57339eb
tienvuiet <tienxinhzai241@GMAIL.COM>
2024-11-19 14:02:05 +0700

    basic5

---
+++ basic5.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a;	
+    printf("nhap vao gia tri cua canh hinh vuong a: ");
+	scanf("%d", &a);
+	int c=a*4 ;
+	printf("chu vi cua hinh vuong la %d\n ",c);   
+	int d=a*a;
+	printf("dien tich cua hinh vuong la %d\n ",d);  
+	return 0; 
+} 
