404d32fa0bc3044f8c37b25f54175ded25da9d15
tienvuiet <tienxinhzai241@GMAIL.COM>
2024-11-19 13:38:01 +0700

    basic1

---
+++ basic1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a;   //kieu du lieu so nguyen
+	char b;  // kieu du lieu ki tu
+	float c;  //kieu du lieu so thuc 
+	double d;  //kieu du lieu so thuc 
+	return 0;
+	
+}
